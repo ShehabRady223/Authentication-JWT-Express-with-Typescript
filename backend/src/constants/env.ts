4c8179ebd9d31497727162dbea8fc06275eda55b
ShehabRady223 <shehabrady72@gmail.com>
2025-12-22 17:21:54 +0200

    Db Config / Middlewares / Error Handeling

---
+++ backend/src/constants/env.ts
@@ -0,0 +1,17 @@
+const getEnv = (key: string, defaultValue?: string): string => {
+    const value = process.env[key] || defaultValue;
+    if (value === undefined){
+        // console.log(value);
+        throw Error(`Missing enviroment variable ${key}`);
+    }
+    return value;
+}
+
+export const MONGO_URL = getEnv("MONGO_URL")
+export const PORT = getEnv("PORT","4004")
+export const NODE_ENV = getEnv("NODE_ENV","development")
+export const APP_ORGIN = getEnv("APP_ORGIN")
+export const JWT_SECRET = getEnv("JWT_SECRET")
+export const JWT_REFRESH_SECRET = getEnv("JWT_REFRESH_SECRET")
+export const EMAIL_SENDER = getEnv("EMAIL_SENDER")
+export const RESEND_API_KEY = getEnv("RESEND_API_KEY")
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,29 @@
-import  express  from "express";
+import "dotenv/config"
+import express from "express";
+import connectToDatabase from "./config/db.js";
+import { APP_ORGIN, PORT } from "./constants/env.js";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
-app.listen(4004,()=>{
+//Middilewares
+app.use(express.json())
+app.use(cors({
+    origin:APP_ORGIN,
+    credentials:true
+}))
+app.use(cookieParser())
+
+//Routers
+// app.get('/')
+
+//Error Exapction
+app.use(errorHandler)
+
+//Running App
+app.listen(PORT, async () => {
     console.log("Server Working Good!!");
+    await connectToDatabase()
 })
