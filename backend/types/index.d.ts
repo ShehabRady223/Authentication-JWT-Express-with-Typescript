80c6f521cd39be7ed866d54d6c728f340cc8f5c1
ShehabRady223 <shehabrady72@gmail.com>
2025-12-31 11:44:24 +0200

    add types file to fix error in req.* type

---
+++ backend/types/index.d.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+declare module "express-serve-static-core" {
+    interface Request {
+        userId?: mongoose.Types.ObjectId;
+        sessionId?: mongoose.Types.ObjectId;
+    }
+}
+
+export { };
